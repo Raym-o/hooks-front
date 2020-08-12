32c49c24cacd957d22be75c9108c07ad7061a446
Raymond Weiss <RaymondaWeiss@gmail.com>
2020-08-12 17:05:20 -0500

    Added new Update constants to the Constants file

--- src/ProfilePage/ProfilePage.jsx
+++ src/ProfilePage/ProfilePage.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
+import { UpdateForm } from '../UpdateForm';
 
 function ProfilePage() {
+  const [editingAddress, setEditingAddress] = useState(false);
   const user = useSelector(state => state.authentication.user);
+
+  function toggleEditingAddress() {
+    setEditingAddress(!editingAddress);
+  }
+
 
   return (
     <div className="container">
       <h1>Profile</h1>
+      <button>Edit</button>
       <ul>
         <li>First Name: {user.f_name}</li>
         <li>Last Name: {user.l_name}</li>
         <li>Username: {user.username}</li>
       </ul>
+
       <h2>Address</h2>
-      {user && user.address ?
+      <button onClick={toggleEditingAddress}>{user && user.address ? "Edit" : "Add"}</button>
+      {user && user.address && !editingAddress ?
         <ul>
           <li>Line 1: {user.address.line_1}</li>
           <li>Line 2: {user.address.line_2}</li>
@@ -24,7 +34,10 @@ function ProfilePage() {
           <li>Province: {user.address.province_id}</li>
         </ul>
         :
-        <p>No Address Currently Listed</p>
+        user && editingAddress ?
+          <UpdateForm user={user} toggleState={editingAddress} />
+          :
+          <p>No Address Currently Listed</p>
       }
 
       <Link to="/">Return to Landing Page</Link>
