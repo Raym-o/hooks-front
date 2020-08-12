d2d0add8445ba56b7cf0dac4500bf8811b52eb9c
Raymond Weiss <RaymondaWeiss@gmail.com>
2020-08-11 19:45:29 -0500

    Added ProfilePage component for displaying the user details of current logged in user

--- src/ProfilePage/ProfilePage.jsx
+++ src/ProfilePage/ProfilePage.jsx
@@ -14,6 +14,19 @@ function ProfilePage() {
         <li>Last Name: {user.l_name}</li>
         <li>Username: {user.username}</li>
       </ul>
+      <h2>Address</h2>
+      {user && user.address ?
+        <ul>
+          <li>Line 1: {user.address.line_1}</li>
+          <li>Line 2: {user.address.line_2}</li>
+          <li>City: {user.address.city}</li>
+          <li>Postal Code: {user.address.postal_code}</li>
+          <li>Province: {user.address.province_id}</li>
+        </ul>
+        :
+        <p>No Address Currently Listed</p>
+      }
+
       <Link to="/">Return to Landing Page</Link>
     </div>
   );
