18c381165884221e18a26970ce578188a0bdd3a5
Raymond Weiss <RaymondaWeiss@gmail.com>
2020-08-13 16:48:16 -0500

    In submitHandler, moved setSubmitted hook call out of success condition, so it affects form validation events

--- src/UpdateForm/UpdateForm.jsx
+++ src/UpdateForm/UpdateForm.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 
 import { userActions } from '../_actions';
 
+
 function UpdateForm(props) {
   const dispatch = useDispatch();
   const [inputs, setInputs] = useState({
@@ -24,18 +25,15 @@ function UpdateForm(props) {
   function handleSubmit(e) {
     e.preventDefault();
 
+    setSubmitted(true);
     if (line_1 && postal_code && province_id) {
-      setSubmitted(true);
       const user = {
         ...props.user,
         address: inputs
       };
-      const { password_digest, created_at, updated_at, ...userClean } = user;
-      dispatch(userActions.updateAddress(userClean));
+      const { password_digest, created_at, updated_at, ...userParsed } = user;
+      dispatch(userActions.updateAddress(userParsed));
       props.toggle();
-    } else {
-      setSubmitted(false);
-      alert("missing required fields!");
     }
   }
 
@@ -47,29 +45,40 @@ function UpdateForm(props) {
 
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="line_1">Line 1</label>
-        <input type="text" className="form-control" name="line_1" value={line_1 ? line_1 : ''} onChange={formFieldChangeHandler} id="line_1" ></input>
-      </div>
-      <div className="form-group">
-        <label htmlFor="line_2">Line 2</label>
-        <input type="text" className="form-control" name="line_2" value={line_2 ? line_2 : ''} onChange={formFieldChangeHandler} id="line_2" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="city">City</label>
-        <input type="text" className="form-control" name="city" value={city ? city : ''} onChange={formFieldChangeHandler} id="city" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="postal_code">Postal Code</label>
-        <input type="text" className="form-control" name="postal_code" value={postal_code ? postal_code : ''} onChange={formFieldChangeHandler} id="postal_code" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="province_id">Province</label>
-        <input type="text" className="form-control" name="province_id" value={province_id ? province_id : ''} onChange={formFieldChangeHandler} id="province_id" />
-      </div>
-      <button type="submit" className="btn btn-primary">Submit</button>
-    </form>
+    <div className="col-lg-8 offset-lg-2">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="line_1">Line 1</label>
+          <input type="text" className={'form-control' + (submitted && !line_1 ? ' is-invalid' : '')} name="line_1" value={line_1 ? line_1 : ''} onChange={formFieldChangeHandler} id="line_1" ></input>
+          {submitted && (!line_1 || line_1 === '') &&
+            <div className="invalid-feedback">Line 1 is required</div>
+          }
+        </div>
+        <div className="form-group">
+          <label htmlFor="line_2">Line 2</label>
+          <input type="text" className="form-control" name="line_2" value={line_2 ? line_2 : ''} onChange={formFieldChangeHandler} id="line_2" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="city">City</label>
+          <input type="text" className="form-control" name="city" value={city ? city : ''} onChange={formFieldChangeHandler} id="city" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="postal_code">Postal Code</label>
+          <input type="text" className={'form-control' + (submitted && !postal_code ? ' is-invalid' : '')} name="postal_code" value={postal_code ? postal_code : ''} onChange={formFieldChangeHandler} id="postal_code" />
+          {submitted && (!postal_code || postal_code === '') &&
+            <div className="invalid-feedback">Postal Code is required</div>
+          }
+        </div>
+        <div className="form-group">
+          <label htmlFor="province_id">Province</label>
+          <input type="text" className={'form-control' + (submitted && !province_id ? ' is-invalid' : '')} name="province_id" value={province_id ? province_id : ''} onChange={formFieldChangeHandler} id="province_id" />
+          {submitted && (!province_id || province_id === '') &&
+            <div className="invalid-feedback">Province is required</div>
+          }
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
   );
 }
 
