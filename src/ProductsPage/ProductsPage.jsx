ae854f42f9be660200522cd4032aac425ecc6c04
Raymond Weiss <RaymondaWeiss@gmail.com>
2020-08-20 03:27:00 -0500

    Added logic for pagination value

--- src/ProductsPage/ProductsPage.jsx
+++ src/ProductsPage/ProductsPage.jsx
@@ -7,12 +7,33 @@ function ProductsPage() {
   const products = useSelector(state => state.products);
   const dispatch = useDispatch();
 
+  const { items, productCount } = products;
+
+  const pageCount = getPages(productCount);
+
+  function getPages(productCount) {
+    const remainder = productCount % 10;
+    const extraPage = remainder > 0 ? 1 : 0;
+    const totalPages = ((productCount - remainder) / 10) + extraPage;
+    return totalPages;
+  }
+  console.log(items);
   useEffect(() => {
     dispatch(productActions.getAll())
   }, [])
 
   return (
-    <div>Products Page</div>
+    <div>
+      <div>Products Page</div>
+      <div>
+        {items.map(item => {
+          return (
+            <p>{item.title}</p>
+          )
+        })}
+      </div>
+    </div>
+
   );
 }
 
