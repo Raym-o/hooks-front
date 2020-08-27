3d8ed6e9a707674fe153650854007209638e23cd
Raymond Weiss <RaymondaWeiss@gmail.com>
2020-08-27 16:35:54 -0500

    I have begun adding SingleProduct component display on click of list item button from products#index view.

--- src/ProductsPage/ProductsPage.jsx
+++ src/ProductsPage/ProductsPage.jsx
@@ -2,19 +2,45 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { productActions } from '../_actions';
+import { productConstants } from '../_constants';
 
 import { ProductCard, Pagination, RowContainer } from '../_components';
 
+import { SingleProductPage } from '../SingleProductPage';
+
 function ProductsPage() {
   const products = useSelector(state => state.products);
+
   const dispatch = useDispatch();
-  const { items, productCount, offset } = products;
+
+  const { items, productCount, offset, singleProductId } = products;
+
+  // 1-10 based not 0-9
+  const indexOfSingleProduct = singleProductId - Number(offset);
+
+  let singleProduct =
+    indexOfSingleProduct ?
+      items[indexOfSingleProduct - 1]
+      :
+      null;
+
 
   useEffect(() => {
+    console.log(singleProduct);
     dispatch(productActions.getAll(offset))
   }, [])
 
   let returnedVal = <p></p>;
+  if (items && singleProductId) {
+    return (
+      <div>
+        {singleProduct && <SingleProductPage productDetails={singleProduct} />}
+        <button onClick={() => {
+          dispatch({ type: productConstants.PRODUCTS_STATE_SET_SINGLE, singleProductId: null })
+        }} >RESET</button>
+      </div>
+    )
+  }
   if (items) {
     returnedVal =
       <div className="container">
