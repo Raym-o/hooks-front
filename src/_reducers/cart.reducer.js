dab66ed5ee25580e7613e7d393d7df297491d8f7
Raymond Weiss <RaymondaWeiss@gmail.com>
2020-08-27 18:36:07 -0500

    Made changes to cart.reducer.js, cart_state is now an array, also added Remove function which filters on ID

--- src/_reducers/cart.reducer.js
+++ src/_reducers/cart.reducer.js
@@ -1,14 +1,25 @@
 import { cartConstants } from '../_constants';
 
 let cartStorage = JSON.parse(localStorage.getItem('cart'));
-const initialState = cartStorage ? cartStorage : {};
+const initialState = cartStorage ? cartStorage : [];
 
 export function cart(state = initialState, action) {
   switch (action.type) {
+    // code written to allow single product of type, but eventually allow multiples 
     case cartConstants.UPDATE_CART:
-      return {
-        cart: action.cart
+      if (!state.includes(action.productUpdate)) {
+        return (
+          [...state,
+          action.productUpdate
+          ]
+        );
+      } else {
+        return state;
       };
+    case cartConstants.REMOVE_PRODUCT:
+      return [
+        ...state.filter(p => p.id !== action.id)
+      ];
     default:
       return state
   }
