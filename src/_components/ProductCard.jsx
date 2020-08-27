98139a5aa5ecf8a17215ecb42c2aa6919fdd6dd4
Raymond Weiss <RaymondaWeiss@gmail.com>
2020-08-27 16:39:15 -0500

    Added navigation to SingleProduct on button click of each ProductCard in listview. Removed some unneeded code and added some basic styling to make it bearable to look at :o

--- src/_components/ProductCard.jsx
+++ src/_components/ProductCard.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
-import config from 'config';
+import { productConstants } from '../_constants'
+
+const cardStyle = {
+  paddingTop: 5
+};
+const btnStyle = {
+  position: "relative",
+  float: "right",
+  marginBottom: "5px"
+};
+const titleStyle = {
+  minHeight: '8em'
+}
 
 
 export const ProductCard = ({ productDetails }) => {
-
+  const dispatch = useDispatch();
   const product = productDetails.product;
   const imagesUrls = productDetails.images_urls;
 
-  const cardStyle = {
-    paddingTop: 5
-  };
-
-  const btnStyle = {
-    position: "relative",
-    float: "right",
-    marginBottom: "5px"
-  };
-
-  const titleStyle = {
-    minHeight: '8em'
+  const handleButtonClick = (event, value = event.target.id) => {
+    dispatch({ type: productConstants.PRODUCTS_STATE_SET_SINGLE, singleProductId: value })
   }
+
 
   if (product) {
     return (
@@ -29,7 +33,7 @@ export const ProductCard = ({ productDetails }) => {
           <img src={imagesUrls[0] ? imagesUrls[0] : 'public/default_jtg_image.jpeg'} className="card-img-top" alt={product.title} />
           <div className="card-body">
             <h6 style={titleStyle} className="card-title">{product.title}</h6>
-            <a href="#" style={btnStyle} className="btn btn-light">VIEW</a>
+            <button id={product.id} onClick={(event) => handleButtonClick(event)} style={btnStyle} className="btn btn-light">VIEW</button>
           </div>
         </div >
       </div>
