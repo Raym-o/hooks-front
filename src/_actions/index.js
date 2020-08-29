534424d52d1b8d6d64140a19e694211a91e428d2
Raymond Weiss <RaymondaWeiss@gmail.com>
2020-08-29 01:22:46 -0500

    Added references to *checkout* and *province* actions

--- src/_actions/index.js
+++ src/_actions/index.js
@@ -2,3 +2,5 @@ export * from './alert.actions';
 export * from './user.actions';
 export * from './product.actions';
 export * from './cart.actions';
+export * from './checkout.actions';
+export * from './province.actions';
